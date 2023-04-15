a787dc01fc24996b9e0609531260fc2be5111b19
DS\PTFK76 <ptfk76@motorolasoultions.com>
2023-04-15 19:42:12 +0200

    resource handling + gitignore

--- PlayMaker/PlayMaker/PlayMakerApp/Program.cs
+++ PlayMaker/PlayMaker/PlayMakerApp/Program.cs
@@ -1,8 +1,21 @@
+using Microsoft.AspNetCore.Localization;
+using Microsoft.Extensions.Localization;
+using System.Globalization;
+using System.Reflection;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddControllersWithViews();
-
+builder.Services.AddLocalization(options => options.ResourcesPath = "C:\\C_Sources\\PlayMaker2\\PlayMaker\\PlayMaker\\Resources");
+//C:\\C_Sources\\PlayMaker2\\PlayMaker\\PlayMaker\\Resources\\
+builder.Services.AddControllersWithViews().AddDataAnnotationsLocalization(options =>
+{
+    var type = typeof(Resources.Common);
+    var assembly = new AssemblyName(type.GetTypeInfo().Assembly.FullName);
+    var factory = builder.Services.BuildServiceProvider().GetService<IStringLocalizerFactory>();
+    var localizer = factory.Create("Common", assembly.Name);
+    options.DataAnnotationLocalizerProvider = (t, f) => localizer;  
+});
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -12,14 +25,22 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+var supportedCultures = new[]
+{
+    new CultureInfo("en"),
+    new CultureInfo("pl")
+};
+app.UseRequestLocalization(new RequestLocalizationOptions
+{
+    DefaultRequestCulture = new RequestCulture("en"),
+    SupportedCultures = supportedCultures,
+    SupportedUICultures = supportedCultures
+});
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
-
 app.UseRouting();
-
 app.UseAuthorization();
-
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
