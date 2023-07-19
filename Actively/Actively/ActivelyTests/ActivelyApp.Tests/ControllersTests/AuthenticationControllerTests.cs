2abe92f9c13d0db1e2b27e26e75ca7db4b3f3d35
DS\PTFK76 <ptfk76@motorolasoultions.com>
2023-07-20 00:16:43 +0200

    authentication tests

--- Actively/Actively/ActivelyTests/ActivelyApp.Tests/ControllersTests/AuthenticationControllerTests.cs
+++ Actively/Actively/ActivelyTests/ActivelyApp.Tests/ControllersTests/AuthenticationControllerTests.cs
@@ -13,6 +13,7 @@ using Moq;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Http;
 using ActivelyApp.Models.Authentication.Registration;
+using ActivelyApp.Models.Authentication.Login;
 
 namespace ActivelyApp.Tests.ControllersTests
 {
@@ -35,7 +36,7 @@ namespace ActivelyApp.Tests.ControllersTests
             _roleManagerMock = new Mock<RoleManager<IdentityRole>>(roleStore.Object, null, null, null, null);
 
             _controller = new AuthenticationController(_userManagerMock.Object, _roleManagerMock.Object,
-                 _emailServiceMock.Object, _signInManagerMock.Object, _configurationMock.Object);
+                 _emailServiceMock.Object, _signInManagerMock.Object, _configurationMock.Object);  
         }
 
         [Theory]
@@ -66,8 +67,9 @@ namespace ActivelyApp.Tests.ControllersTests
         }
 
         [Theory]
-        //[InlineData(false, true, true)] // positive scenario
         [InlineData(true, true, true)] // negative scenario
+        [InlineData(false, false, true)] // negative scenario
+        [InlineData(false, true, false)] // negative scenario
         public void RegisterUser_ReturnsProperStatusCode_WhenDataProvided(bool userExist, bool roleExist, bool creationSucces)
         {
             //arrange
@@ -78,26 +80,124 @@ namespace ActivelyApp.Tests.ControllersTests
                 ConfirmPassword = "Test",
                 Password = "Test",
             };
+            var createdUser = new IdentityUser("TestUser")
+            {
+                Email = "test@test.com",
+                SecurityStamp = Guid.NewGuid().ToString(),
+                EmailConfirmed = false,
+            };
+
+            _userManagerMock.Setup(x => x.GenerateEmailConfirmationTokenAsync(It.IsAny<IdentityUser>())).ReturnsAsync("token");
 
             if (userExist)
             {
-                _userManagerMock.Setup(x => x.FindByEmailAsync(It.IsAny<string>())).ReturnsAsync(new IdentityUser("TestUser")
-                {
-                    Email = "test@test.com",
-                    SecurityStamp = Guid.NewGuid().ToString(),
-                    EmailConfirmed = false,
-                });
+                _userManagerMock.Setup(x => x.FindByEmailAsync(It.IsAny<string>())).ReturnsAsync(createdUser);
             }
             else
             {
-                _userManagerMock.Setup(x => x.FindByEmailAsync(It.IsAny<string>())).Returns(new Task<IdentityUser>(null));
+                IdentityUser user = null;
+                _userManagerMock.Setup(x => x.FindByEmailAsync(It.IsAny<string>())).ReturnsAsync(user);
             }
 
+            if (roleExist)
+            {
+                _roleManagerMock.Setup(r => r.RoleExistsAsync(It.IsAny<string>())).ReturnsAsync(true);
+            }
+            else
+                _roleManagerMock.Setup(r => r.RoleExistsAsync(It.IsAny<string>())).ReturnsAsync(false);
+
+
+            if (creationSucces)
+            {
+                _userManagerMock.Setup(x => x.CreateAsync(It.IsAny<IdentityUser>(), It.IsAny<string>())).ReturnsAsync(IdentityResult.Success);
+            }
+            else 
+            {
+                _userManagerMock.Setup(x => x.CreateAsync(It.IsAny<IdentityUser>(), It.IsAny<string>())).ReturnsAsync(IdentityResult.Failed());
+            }
+                
+
+            //act
             var result = _controller.Register(model).Result as ObjectResult;
 
+            //assert
             if (userExist)
             {
                 Assert.Equal(StatusCodes.Status403Forbidden, result?.StatusCode);
+            }
+            else
+            {
+                if(!roleExist) 
+                {
+                    Assert.Equal(StatusCodes.Status500InternalServerError, result?.StatusCode);
+                }
+                else
+                {
+                    if (creationSucces)
+                    {
+                        Assert.Equal(StatusCodes.Status201Created, result?.StatusCode);
+                    }
+                    else
+                    {
+                        Assert.Equal(StatusCodes.Status500InternalServerError, result?.StatusCode);
+                    }
+                }
+            }
+        }
+
+        [Theory]
+        [InlineData(true, true, true, true)]
+        [InlineData(true, false, true, true)]
+        [InlineData(false, false, true, true)]
+        [InlineData(true, false, true, false)]
+        [InlineData(false, false, false, false)]
+        [InlineData(true, false, false, true)]
+        public void Login_ReturnsProperStatusCode_WhenDataProvided(bool userExist, bool twoFactorAuthEnabled, bool correctPass, bool emailConfirmed)
+        {
+            //arrange
+            LoginModel model = new LoginModel
+            {
+                UserName = "TestUserName",
+                Password = "Test",
+            };
+
+            var createdUser = new IdentityUser("TestUserName")
+            {
+                Email = "test@test.com",
+                SecurityStamp = Guid.NewGuid().ToString(),
+                EmailConfirmed = emailConfirmed,
+                TwoFactorEnabled = twoFactorAuthEnabled,
+            };
+            _userManagerMock.Setup(x => x.GenerateTwoFactorTokenAsync(It.IsAny<IdentityUser>(), It.IsAny<string>())).ReturnsAsync("token");
+            if (userExist)
+            {
+                _userManagerMock.Setup(x => x.FindByNameAsync(It.IsAny<string>())).ReturnsAsync(createdUser);
+            }
+            else
+            {
+                IdentityUser user = null;
+                _userManagerMock.Setup(x => x.FindByNameAsync(It.IsAny<string>())).ReturnsAsync(user);
+            }
+
+            _userManagerMock.Setup(x => x.CheckPasswordAsync(It.IsAny<IdentityUser>(), It.IsAny<string>())).ReturnsAsync(correctPass);
+            _userManagerMock.Setup(x => x.IsEmailConfirmedAsync(It.IsAny<IdentityUser>())).ReturnsAsync(emailConfirmed);
+            _userManagerMock.Setup(x => x.GetRolesAsync(It.IsAny<IdentityUser>())).ReturnsAsync(new List<string>()
+            {
+                "Admin"
+            });
+            _configurationMock.Setup(x => x[It.IsAny<string>()]).Returns("this is my custom JWT Symmetric key for authentication");
+
+            //act
+            var result = _controller.Login(model).Result as ObjectResult;
+            
+            //assert
+            if (userExist && correctPass && emailConfirmed)
+            {
+                Assert.Equal(StatusCodes.Status200OK, result?.StatusCode);
+            }
+            else
+            {
+                Assert.Equal(StatusCodes.Status401Unauthorized, result?.StatusCode);
             }
         }
     }
