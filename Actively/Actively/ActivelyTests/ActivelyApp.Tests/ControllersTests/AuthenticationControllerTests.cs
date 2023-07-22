b10957bf8df8d8ee1cf00acd9b325555036bd9b2
Woselko <wojciech.wlas@outlook.com>
2023-07-23 00:16:30 +0200

    test refactor + testcases added

--- Actively/Actively/ActivelyTests/ActivelyApp.Tests/ControllersTests/AuthenticationControllerTests.cs
+++ Actively/Actively/ActivelyTests/ActivelyApp.Tests/ControllersTests/AuthenticationControllerTests.cs
@@ -1,7 +1,7 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
-using Microsoft.SqlServer.Management.Smo;
+//using Microsoft.SqlServer.Management.Smo;
 using Newtonsoft.Json.Linq;
 using System.Text;
 using Xunit;
@@ -14,6 +14,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Http;
 using ActivelyApp.Models.Authentication.Registration;
 using ActivelyApp.Models.Authentication.Login;
+using ActivelyDomain.Entities;
 
 namespace ActivelyApp.Tests.ControllersTests
 {
@@ -21,8 +22,8 @@ namespace ActivelyApp.Tests.ControllersTests
     {
         private Mock<IConfiguration>? _configurationMock;
         private Mock<IEmailService>? _emailServiceMock;
-        private Mock<UserManager<IdentityUser>>? _userManagerMock;
-        private Mock<SignInManager<IdentityUser>>? _signInManagerMock;
+        private Mock<UserManager<User>>? _userManagerMock;
+        private Mock<SignInManager<User>>? _signInManagerMock;
         private Mock<RoleManager<IdentityRole>>? _roleManagerMock;
         private AuthenticationController? _controller;
 
@@ -31,8 +32,9 @@ namespace ActivelyApp.Tests.ControllersTests
             var roleStore = new Mock<IRoleStore<IdentityRole>>();
             _configurationMock = new Mock<IConfiguration>();
             _emailServiceMock = new Mock<IEmailService>();
-            _userManagerMock = new Mock<UserManager<IdentityUser>>(Mock.Of<IUserStore<IdentityUser>>(), null, null, null, null, null, null, null, null);
-            _signInManagerMock = new Mock<SignInManager<IdentityUser>>(_userManagerMock.Object, Mock.Of<IHttpContextAccessor>(), Mock.Of<IUserClaimsPrincipalFactory<IdentityUser>>(), null, null, null, null);
+            _userManagerMock = new Mock<UserManager<User>>(Mock.Of<IUserStore<User>>(), null, null, null, null, null, null, null, null);
+            _signInManagerMock = new Mock<SignInManager<User>>(_userManagerMock.Object, Mock.Of<IHttpContextAccessor>(), 
+                Mock.Of<IUserClaimsPrincipalFactory<User>>(), null, null, null, null);
             _roleManagerMock = new Mock<RoleManager<IdentityRole>>(roleStore.Object, null, null, null, null);
 
             _controller = new AuthenticationController(_userManagerMock.Object, _roleManagerMock.Object,
@@ -45,16 +47,17 @@ namespace ActivelyApp.Tests.ControllersTests
         public void ConfirmEmail_ReturnsProperResponse_WhenDataIsGiven(string token, string email, bool shouldSuccess)
         {
             //arrange
-            _userManagerMock.Setup(x => x.FindByEmailAsync(It.IsAny<string>())).ReturnsAsync(new IdentityUser("TestUser")
+            _userManagerMock.Setup(x => x.FindByEmailAsync(It.IsAny<string>())).ReturnsAsync(new User()
             {
+                UserName = "TestUser",
                 Email = email,
                 SecurityStamp = Guid.NewGuid().ToString(),
                 EmailConfirmed = false,
             });
             if (shouldSuccess)
-                _userManagerMock.Setup(x => x.ConfirmEmailAsync(It.IsAny<IdentityUser>(), It.IsAny<string>())).ReturnsAsync(IdentityResult.Success);
+                _userManagerMock.Setup(x => x.ConfirmEmailAsync(It.IsAny<User>(), It.IsAny<string>())).ReturnsAsync(IdentityResult.Success);
             else
-                _userManagerMock.Setup(x => x.ConfirmEmailAsync(It.IsAny<IdentityUser>(), It.IsAny<string>())).ReturnsAsync(IdentityResult.Failed());
+                _userManagerMock.Setup(x => x.ConfirmEmailAsync(It.IsAny<User>(), It.IsAny<string>())).ReturnsAsync(IdentityResult.Failed());
 
             //act
             var result = _controller.ConfirmEmail(token, email).Result as ObjectResult;
@@ -80,14 +83,15 @@ namespace ActivelyApp.Tests.ControllersTests
                 ConfirmPassword = "Test",
                 Password = "Test",
             };
-            var createdUser = new IdentityUser("TestUser")
+            var createdUser = new User()
             {
+                UserName = "TestUser",
                 Email = "test@test.com",
                 SecurityStamp = Guid.NewGuid().ToString(),
                 EmailConfirmed = false,
             };
 
-            _userManagerMock.Setup(x => x.GenerateEmailConfirmationTokenAsync(It.IsAny<IdentityUser>())).ReturnsAsync("token");
+            _userManagerMock.Setup(x => x.GenerateEmailConfirmationTokenAsync(It.IsAny<User>())).ReturnsAsync("token");
 
             if (userExist)
             {
@@ -95,7 +99,7 @@ namespace ActivelyApp.Tests.ControllersTests
             }
             else
             {
-                IdentityUser user = null;
+                User user = null;
                 _userManagerMock.Setup(x => x.FindByEmailAsync(It.IsAny<string>())).ReturnsAsync(user);
             }
 
@@ -109,11 +113,11 @@ namespace ActivelyApp.Tests.ControllersTests
 
             if (creationSucces)
             {
-                _userManagerMock.Setup(x => x.CreateAsync(It.IsAny<IdentityUser>(), It.IsAny<string>())).ReturnsAsync(IdentityResult.Success);
+                _userManagerMock.Setup(x => x.CreateAsync(It.IsAny<User>(), It.IsAny<string>())).ReturnsAsync(IdentityResult.Success);
             }
             else 
             {
-                _userManagerMock.Setup(x => x.CreateAsync(It.IsAny<IdentityUser>(), It.IsAny<string>())).ReturnsAsync(IdentityResult.Failed());
+                _userManagerMock.Setup(x => x.CreateAsync(It.IsAny<User>(), It.IsAny<string>())).ReturnsAsync(IdentityResult.Failed());
             }
                 
 
@@ -161,27 +165,28 @@ namespace ActivelyApp.Tests.ControllersTests
                 Password = "Test",
             };
 
-            var createdUser = new IdentityUser("TestUserName")
+            var createdUser = new User()
             {
+                UserName = "TestUserName",
                 Email = "test@test.com",
                 SecurityStamp = Guid.NewGuid().ToString(),
                 EmailConfirmed = emailConfirmed,
                 TwoFactorEnabled = twoFactorAuthEnabled,
             };
-            _userManagerMock.Setup(x => x.GenerateTwoFactorTokenAsync(It.IsAny<IdentityUser>(), It.IsAny<string>())).ReturnsAsync("token");
+            _userManagerMock.Setup(x => x.GenerateTwoFactorTokenAsync(It.IsAny<User>(), It.IsAny<string>())).ReturnsAsync("token");
             if (userExist)
             {
                 _userManagerMock.Setup(x => x.FindByNameAsync(It.IsAny<string>())).ReturnsAsync(createdUser);
             }
             else
             {
-                IdentityUser user = null;
+                User user = null;
                 _userManagerMock.Setup(x => x.FindByNameAsync(It.IsAny<string>())).ReturnsAsync(user);
             }
 
-            _userManagerMock.Setup(x => x.CheckPasswordAsync(It.IsAny<IdentityUser>(), It.IsAny<string>())).ReturnsAsync(correctPass);
-            _userManagerMock.Setup(x => x.IsEmailConfirmedAsync(It.IsAny<IdentityUser>())).ReturnsAsync(emailConfirmed);
-            _userManagerMock.Setup(x => x.GetRolesAsync(It.IsAny<IdentityUser>())).ReturnsAsync(new List<string>()
+            _userManagerMock.Setup(x => x.CheckPasswordAsync(It.IsAny<User>(), It.IsAny<string>())).ReturnsAsync(correctPass);
+            _userManagerMock.Setup(x => x.IsEmailConfirmedAsync(It.IsAny<User>())).ReturnsAsync(emailConfirmed);
+            _userManagerMock.Setup(x => x.GetRolesAsync(It.IsAny<User>())).ReturnsAsync(new List<string>()
             {
                 "Admin"
             });
