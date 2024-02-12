8350b936c6336a7af7a60c05ed05c290390d2c6b
EmilBulka <74752413+EmilBulka@users.noreply.github.com>
2024-02-12 21:59:12 +0100

    implemented error handling middleware and added into middleware flow (#9)

---
+++ Actively/Actively/ActivelyApp/Middleware/ErrorHandlingMiddleware.cs
@@ -0,0 +1,35 @@
+﻿using ActivelyApp.Models.Common;
+using Resources;
+
+namespace ActivelyApp.Middleware
+{
+	public class ErrorHandlingMiddleware : IMiddleware
+	{
+		private readonly ILogger<ErrorHandlingMiddleware> _logger;
+		public ErrorHandlingMiddleware(ILogger<ErrorHandlingMiddleware> logger)
+		{
+			_logger = logger;
+		}
+		public async Task InvokeAsync(HttpContext context, RequestDelegate next)
+		{
+			try
+			{
+				await next.Invoke(context);
+			}
+			catch (Exception e)
+			{
+				_logger.LogError(e, e.Message); //here need to be changed after implementation of nlog 
+				context.Response.StatusCode = 500;
+				context.Response.ContentType = "application/json";
+				var errorDetails = new
+				{
+					Type = ResponseType.Error,
+					Status = Common.Error,
+					Message = "Something went wrong",
+					IsSuccess = false
+				};
+				await context.Response.WriteAsJsonAsync(errorDetails);
+			};			
+		}
+	}
+}
